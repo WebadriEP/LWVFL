f9d8f21f2b88d9bf61df06716187c8296b2585e2
Parker Miller <Parkerstar11@outlook.coom>
2022-09-29 13:45:07 -0400

    Frontend Member Pages/Components

---
+++ frontend/src/components/member/MemberList.js
@@ -0,0 +1,21 @@
+//components
+
+import EngagementList from '../engagement/EngagementList';
+import MemberListItem from './MemberListItem';
+
+const MemberList = () => {
+    return (
+
+        <>
+        <h2>Member 1</h2>
+        <MemberListItem />
+
+        <h2>Member2</h2>
+        <MemberListItem />
+
+        </>
+    );
+
+}
+
+export default MemberList
