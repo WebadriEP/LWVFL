29ec310781f8e1311ecaf3a8af9b765146848249
jeblax <jakeburns1291999@gmail.com>
2022-09-29 12:33:41 -0400

    user update

---
+++ backend/models/userModel.js
@@ -0,0 +1,19 @@
+const userSchema = new Schema({
+    
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    
+    
+  })
+  
