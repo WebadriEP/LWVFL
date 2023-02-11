997532aa82116821e786ff0088c88a8fd94e56aa
ryandey <techrpd15113@gmail.com>
2023-02-10 19:43:43 -0500

    Initial implementation

--- frontend/src/pages/Engagement.js
+++ frontend/src/pages/Engagement.js
@@ -1,29 +1,50 @@
 import { Heading, Box, Stack } from '@chakra-ui/react';
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { getAllMembers } from '../api/axios';
+import { Link } from 'react-router-dom';
+
+// css
+import '../components/engagement/EngagementStyles.css'
 
 // components
-import EngagementList from '../components/engagement/EngagementList';
-import EngagementDatatable from '../components/engagement/EngagementDatatable';
+import EngagementTable from '../components/engagement/EngagementTable';
 
 const Engagement = () => {
+    const [members, setMembers] = useState([]);
+    
     // Fetch all members -- Used for search functionality
-    // useEffect(() => {
-    //     getAllMembers().then(json => {
-    //         // Filter out members marked for engagement
-    //         setMembers(json.filter(member => member.memberStatus === 'engage'))
-    //         //setMembers(json)
-    //     })
-    // }, [])
+    useEffect(() => {
+        getAllMembers().then(json => {
+            // Filter out members marked for engagement
+            setMembers(json.filter(member => member.memberStatus === 'engage'))
+            //setMembers(json)
+        })
+    }, [])
+
+    const columns = useMemo(() => [
+        {
+          Header: 'Name',
+          accessor: 'fullName',
+          Cell: props => <Link to={`/member/${props.row.original._id}`}>{props.value}</Link>,
+          className: 'nameCell'
+        },
+        {
+          Header: 'Email',
+          accessor: 'email',
+        },
+        {
+          Header: 'Phone Number',
+          accessor: 'phone',
+        },
+    ], [])
 
     return (
         <Stack>
             <Heading mb={5}>Engagement List</Heading>
             <Box>
-                <EngagementDatatable />
+                <EngagementTable columns={columns} data={members} />
             </Box>
-            
         </Stack>
     );
 }
