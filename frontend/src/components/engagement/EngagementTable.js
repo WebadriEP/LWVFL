09819250f4a3924e0af72dd508ab7a8d2b8d6b21
ryandey <techrpd15113@gmail.com>
2023-02-12 03:45:41 -0500

    Action button & styled member profile links

--- frontend/src/components/engagement/EngagementTable.js
+++ frontend/src/components/engagement/EngagementTable.js
@@ -1,64 +1,46 @@
-import $ from "jquery";
-import React, { useState, useEffect, useMemo } from "react";
-import { Link } from "react-router-dom";
-import { Table, Tr, Th, Td, Thead, Tbody, Link as ReachLink } from '@chakra-ui/react';
-import { useTable, useSortBy } from 'react-table';
+import { Table, Tr, Th, Td, Thead, Tbody } from "@chakra-ui/react"
+import { useTable, useSortBy } from "react-table"
 
-import "datatables.net";
-import "datatables.net-dt";
-import DataTable from 'datatables.net-dt';
-import 'datatables.net-dt/css/jquery.dataTables.css';
-
-// import datatables styles
-import "../ui/datatableStyles.css";
-
-function EngagementTable ({ columns, data }) {
-
-    const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      rows,
-      prepareRow,
-    } = useTable({
-      columns,
-      data,
-    }, useSortBy)
-
-    return (
-      <Table {...getTableProps()}>
-        <Thead>
-          {headerGroups.map(headerGroup => (
-            <Tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <Th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render('Header')}
-                  <span>
-                    {column.isSorted
-                      ? column.isSortedDesc
-                        ? ' 🔽'
-                        : ' 🔼'
-                      : ''}
-                  </span>
-                </Th>
-              ))}
-            </Tr>
-          ))}
-        </Thead>
-        <Tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row)
-            return (
-              <Tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <Td {...cell.getCellProps()}>{cell.render('Cell')}</Td>
-                })}
-              </Tr>
-            )
-          })}
-        </Tbody>
-      </Table>
+function EngagementTable({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+      },
+      useSortBy
     )
+
+  return (
+    <Table {...getTableProps()}>
+      <Thead>
+        {headerGroups.map((headerGroup) => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render("Header")}
+                <span>
+                  {column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""}
+                </span>
+              </Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row)
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <Td {...cell.getCellProps()}>{cell.render("Cell")}</Td>
+              })}
+            </Tr>
+          )
+        })}
+      </Tbody>
+    </Table>
+  )
 }
 
 export default EngagementTable
