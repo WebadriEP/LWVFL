b12be3fc79b6e7f7a045cf944946baf7133625cb
Ryan Dey <techrpd15113@gmail.com>
2023-03-02 14:51:32 -0500

    CORS fixes for deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // Make life easier by creating a base URL
 export const api = axios.create({
-  baseURL: "https://lwvfl-api.up.railway.app/api",
+  baseURL: "/api",
 })
 
 // Get all members currently stored in the DB
