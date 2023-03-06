bc7a1b933020bed93fa5e83926d41532b221aa8d
jeblax <103952816+jeblax@users.noreply.github.com>
2023-03-06 16:16:14 -0500

    test

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -11,7 +11,7 @@ export const useLogin = () =>{
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch(process.env.BACKEND_URL+'/api/users/login', {
+        const response = await fetch('https://dontra-production.up.railway.app/api/users/login', {
             method: 'POST',
             body: JSON.stringify({email, password}),
             headers: {'Content-Type': 'application/json',
