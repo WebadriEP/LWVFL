312eb05723f32e97d4af86e48bc796d20f93c923
jeblax <103952816+jeblax@users.noreply.github.com>
2023-03-06 14:17:31 -0500

    Fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const app = express()
 
 // Middleware
 app.use(express.json())
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 // Default page
 app.get("/", (req, res) => {
   res.send("Welcome to the DontraCRM API")
