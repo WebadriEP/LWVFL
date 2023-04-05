b9ed9419cfc5cf12595ca25897a9f7fda9d80133
ryandey <techrpd15113@gmail.com>
2023-04-05 15:34:54 -0400

    Member list/modal updates in progress

--- frontend/src/components/members/AddMemberPop.js
+++ frontend/src/components/members/AddMemberPop.js
@@ -1,110 +1,245 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, useDisclosure } from '@chakra-ui/react'
-
-import { useState } from 'react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  Heading,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  SimpleGrid,
+  Box,
+  FormLabel,
+  Input,
+  Flex,
+} from "@chakra-ui/react"
+import { FiPlus } from "react-icons/fi"
+import { useState } from "react"
 import { useEffect } from "react"
 import { useMembersContext } from "../../hooks/useMembersContext"
-import { addMemberFuntion } from '../../api/MemberCRUD'
-
+import { addMemberFuntion } from "../../api/MemberCRUD"
 
 function AddMemberPop() {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [phone, setPhone] = useState('')
-    const [homeAddress, setHomeAddress] = useState('')
-    const [city, setCity] = useState('')
-    const [state, setUsersState] = useState('')
-    const [zip, setZip] = useState('')
-  
-    const [error, setError] = useState(null);   
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [homeAddress, setHomeAddress] = useState("")
+  const [city, setCity] = useState("")
+  const [state, setUsersState] = useState("")
+  const [zip, setZip] = useState("")
+  const [birthMonth, setBirthMonth] = useState("")
+  const [birthDay, setBirthDay] = useState("")
+  const [birthYear, setBirthYear] = useState("")
+  const [gender, setGender] = useState("")
+  const [memberType, setMemberType] = useState("")
+  const [isStudent, setIsStudent] = useState(false)
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        setError(null);
-        try {
-            await addMemberFuntion(firstName, lastName, email, phone, homeAddress, city, state, zip);
-            onClose()
-        } catch (err) {
-            setError(err.message);
-        }
+  const [error, setError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError(null)
+    try {
+      await addMemberFuntion(
+        firstName,
+        lastName,
+        email,
+        phone,
+        homeAddress,
+        city,
+        state,
+        zip
+      )
+      onClose()
+    } catch (err) {
+      setError(err.message)
     }
-
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    return (
-      <>
-        <Button onClick={onOpen}>Add New Member</Button>
-  
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Add New Member's Information</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-            <form className="create" onSubmit={handleSubmit}>
-                <h3>Add a New Member</h3>
-
-                    <label>First Name*</label>
-                    <input 
-                        type="text"
-                        onChange={(e) => setFirstName(e.target.value)} 
-                        value={firstName} 
-                    />
-
-                    <label>Last Name*</label>
-                    <input 
-                        type="text"
-                        onChange={(e) => setLastName(e.target.value)} 
-                        value={lastName} 
-                    />
-                    <label>Email Address*</label>
-                        <input 
-                        type="email"
-                        onChange={(e) => setEmail(e.target.value)} 
-                        value={email} 
-                    />
-                    <label>Phone Number*</label>
-                    <input 
-                        type="text"
-                        onChange={(e) => setPhone(e.target.value)} 
-                        value={phone} 
-                    />
-                    <label>Home Address*</label>
-                    <input 
-                        type="text"
-                        onChange={(e) => setHomeAddress(e.target.value)} 
-                        value={homeAddress} 
-                    />
-                    <label>Address Line 2</label>
-                    <input 
-                        type="text"
-                        onChange={(e) => setHomeAddress(e.target.value)} 
-                        value={homeAddress} 
-                    />
-                    <label>City*</label>
-                    <input 
-                        type="text"
-                        onChange={(e) => setCity(e.target.value)} 
-                        value={city} 
-                    />
-                    <label>State*</label>
-                    <input 
-                        type="text"
-                        onChange={(e) => setUsersState(e.target.value)} 
-                        value={state} 
-                    />
-                    <label>Zip Code*</label>
-                    <input 
-                        type="text"
-                        onChange={(e) => setZip(e.target.value)} 
-                        value={zip} 
-                    />
-                <Button variant='ghost' onClick={handleSubmit}>Submit</Button> <Button colorScheme='blue' mr={3} onClick={onClose}> Close</Button>
-                {error && <div className ="error">{error}</div>}
-                </form>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      </>
-    )
   }
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      {/* New Member button */}
+      <Button onClick={onOpen} rightIcon={<FiPlus />} colorScheme="blue">
+        New Member
+      </Button>
+
+      {/* Modal */}
+      <Modal isOpen={isOpen} onClose={onClose} borderRadius={8} size="full">
+        {/* Background overlay */}
+        <ModalOverlay />
+
+        {/* Modal content */}
+        <ModalContent>
+          {/* Header */}
+          <ModalHeader borderRadius={8}>
+            <Heading size="xl">New Member Form</Heading>
+          </ModalHeader>
+
+          {/* Close button */}
+          <ModalCloseButton />
+
+          {/* Body */}
+          <ModalBody>
+            <form className="create" onSubmit={handleSubmit}>
+              <SimpleGrid columns={2} spacing={4}>
+                {/* First column */}
+                <Flex direction="column" align="space-between" px={4}>
+                  <Heading size="lg" my={4}>
+                    Contact Information
+                  </Heading>
+
+                  {/* Name */}
+                  <SimpleGrid columns={2} spacing={4} mb={4}>
+                    <Box>
+                      <FormLabel>First Name*</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={(e) => setFirstName(e.target.value)}
+                        value={firstName}
+                        placeholder="John"
+                      />
+                    </Box>
+                    <Box>
+                      <FormLabel>Last Name*</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={(e) => setLastName(e.target.value)}
+                        value={lastName}
+                        placeholder="Doe"
+                      />
+                    </Box>
+                  </SimpleGrid>
+                  <Box mb={4}>
+                    <FormLabel>Email Address*</FormLabel>
+                    <Input
+                      type="email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      value={email}
+                      placeholder="john@example.com"
+                    />
+                  </Box>
+                  <Box mb={4}>
+                    <FormLabel>Phone Number*</FormLabel>
+                    <Input
+                      type="text"
+                      onChange={(e) => setPhone(e.target.value)}
+                      value={phone}
+                      placeholder="1234567890"
+                      required
+                    />
+                  </Box>
+                  <SimpleGrid columns={2} spacing={4}>
+                    <Box>
+                      {/* Birthday */}
+                      <FormLabel>Birth Date*</FormLabel>
+                      <Input
+                        type="date"
+                        onChange={(e) => {
+                          let birthdayString = e.target.value
+                          let birthMonth = birthdayString.slice(5, 7)
+                          let birthDay = birthdayString.slice(8, 10)
+                          let birthYear = birthdayString.slice(0, 4)
+
+                          setBirthMonth(birthMonth)
+                          setBirthDay(birthDay)
+                          setBirthYear(birthYear)
+                        }}
+                        required
+                      />
+                    </Box>
+                    {/* Gender */}
+                  </SimpleGrid>
+                  <Box>
+                    <Heading size="lg" my={4}>
+                      Location Information
+                    </Heading>
+                    <Box mb={4}>
+                      <FormLabel>Home Address*</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={(e) => setHomeAddress(e.target.value)}
+                        value={homeAddress}
+                        placeholder="123 Main St"
+                      />
+                    </Box>
+                    <Box mb={4}>
+                      <FormLabel>Address Line 2</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={(e) => setHomeAddress(e.target.value)}
+                        value={homeAddress}
+                        placeholder="Apartment, suite, etc."
+                      />
+                    </Box>
+                    <SimpleGrid columns={3} spacing={4}>
+                      <Box>
+                        <FormLabel>City*</FormLabel>
+                        <Input
+                          type="text"
+                          onChange={(e) => setCity(e.target.value)}
+                          value={city}
+                          placeholder="New York City"
+                        />
+                      </Box>
+                      <Box>
+                        <FormLabel>State*</FormLabel>
+                        <Input
+                          type="text"
+                          onChange={(e) => setUsersState(e.target.value)}
+                          value={state}
+                          placeholder="NY"
+                        />
+                      </Box>
+                      <Box>
+                        <FormLabel>Zip Code*</FormLabel>
+                        <Input
+                          type="text"
+                          onChange={(e) => setZip(e.target.value)}
+                          value={zip}
+                          placeholder="10001"
+                        />
+                      </Box>
+                    </SimpleGrid>
+                  </Box>
+                </Flex>
+
+                {/* Right column */}
+                <Flex direction="column" align="space-between" px={4}>
+                  <Box>
+                    <Heading size="lg" my={4}>
+                      Membership Information
+                    </Heading>
+                    <Box mb={4}>
+                      <FormLabel>Membership Type*</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={(e) => setZip(e.target.value)}
+                        value={zip}
+                        placeholder="10001"
+                      />
+                    </Box>
+                  </Box>
+                </Flex>
+              </SimpleGrid>
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="ghost" variant="ghost" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue" variant="solid" onClick={handleSubmit}>
+              Add Member
+            </Button>
+
+            {error && <div className="error">{error}</div>}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
 export default AddMemberPop
--- frontend/src/pages/Members.js
+++ frontend/src/pages/Members.js
@@ -22,140 +22,117 @@ import { Link } from "react-router-dom"
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons"
 
 // components
-import MemberActionBar from "../components/members/MemberActionBar";
-import MemberDatatable from "../components/members/MemberDatatable";
+import MemberActionBar from "../components/members/MemberActionBar"
+import MemberDatatable from "../components/members/MemberDatatable"
 
-import MemberTable from '../components/members/memberTable';
-import { useTable, useColumns } from 'react-table';
-
-
-
+import MemberTable from "../components/members/memberTable"
+import { useTable, useColumns } from "react-table"
 
 const Members = () => {
-const [members, setMembers] = useState([])
-const [search, setSearch] = useState("")
-const [searchResults, setSearchResults] = useState([])
-const [showColumns, setShowColumns] = useState([]);
+  const [members, setMembers] = useState([])
+  const [search, setSearch] = useState("")
+  const [searchResults, setSearchResults] = useState([])
+  const [showColumns, setShowColumns] = useState([])
 
-// Fetch all members -- Used for search functionality
-useEffect(() => {
-  getAllMembers().then((json) => {
-    // Filter out members marked for engagement
-    setMembers(json)
-    setShowColumns(
-      json.length > 0 ? Object.keys(json[0]).map((key) => key) : []
-    );
-  })
-}, [])
+  // Fetch all members -- Used for search functionality
+  useEffect(() => {
+    getAllMembers().then((json) => {
+      // Filter out members marked for engagement
+      setMembers(json)
+      setShowColumns(
+        json.length > 0 ? Object.keys(json[0]).map((key) => key) : []
+      )
+    })
+  }, [])
 
-// Search functionality
-const handleSearch = (e) => {
-  setSearch(e.target.value)
+  // Search functionality
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
 
-  if (e.target.value === "") {
-    setSearchResults(members)
-    return
+    if (e.target.value === "") {
+      setSearchResults(members)
+      return
+    }
+
+    setSearchResults(
+      members.filter((member) =>
+        member.fullName.toLowerCase().includes(search.toLowerCase())
+      )
+    )
+  }
+  const handleColumnToggle = (e) => {
+    const { checked, name } = e.target
+    if (checked) {
+      setShowColumns([...showColumns, name])
+    } else {
+      setShowColumns(showColumns.filter((col) => col !== name))
+    }
   }
 
-  setSearchResults(
-    members.filter((member) =>
-      member.fullName.toLowerCase().includes(search.toLowerCase())
-    )
+  //   Determines the columns for the table and what is rendered inside each cell
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "fullName",
+        Cell: (props) => (
+          // Takes name as prop and renders it as a styled link to the member's profile
+          <ReachLink
+            as={Link}
+            to={`/member/${props.row.original._id}`}
+            color="blue.500"
+          >
+            {props.value}
+          </ReachLink>
+          //   TODO: Render badges depicting status, isStudent, etc. (use shorthand like a single letter or an icon)
+        ),
+        className: "nameCell",
+      },
+      {
+        // Renders member's email
+        Header: "Email",
+        accessor: "email",
+      },
+      {
+        // Renders member's phone number
+        Header: "Phone Number",
+        accessor: "phone",
+      },
+      {
+        // Location (City, State)
+        Header: "Location",
+        accessor: "location",
+      },
+      {
+        // Renders actions that user can perform, such as deleting or updating
+        Header: " ",
+        Cell: ({ row }) => (
+          <HStack spacing={4} justify="end">
+            <Tooltip label="Edit Member" hasArrow>
+              <IconButton icon={<EditIcon />} colorScheme="blue" size="sm" />
+            </Tooltip>
+            <Tooltip label="Delete Member" hasArrow>
+              <IconButton icon={<DeleteIcon />} colorScheme="red" size="sm" />
+            </Tooltip>
+          </HStack>
+        ),
+      },
+    ],
+    []
+  )
+
+  return (
+    <>
+      <Flex direction="row" justify="space-between">
+        <Heading as="h1" size="xl" mb={4}>
+          Members
+        </Heading>
+        <AddMemberPop />
+      </Flex>
+
+      <MemberTable columns={columns} data={members} />
+    </>
   )
 }
-const handleColumnToggle = (e) => {
-  const { checked, name } = e.target;
-  if (checked) {
-    setShowColumns([...showColumns, name]);
-  } else {
-    setShowColumns(showColumns.filter((col) => col !== name));
-  }
-};
 
-
-
-//   Determines the columns for the table and what is rendered inside each cell
-const columns = useMemo(
-  () => [
-    {
-      Header: "Name",
-      accessor: "fullName",
-      Cell: (props) => (
-        // Takes name as prop and renders it as a styled link to the member's profile
-        <ReachLink
-          as={Link}
-          to={`/member/${props.row.original._id}`}
-          color="blue.500"
-        >
-          {props.value}
-        </ReachLink>
-        //   TODO: Render badges depicting status, isStudent, etc. (use shorthand like a single letter or an icon)
-      ),
-      className: "nameCell",
-    },
-    {
-      // Renders member's email
-      Header: "Email",
-      accessor: "email",
-    },
-    {
-      // Renders member's phone number
-      Header: "Phone Number",
-      accessor: "phone",
-    },
-    {
-      // Location (City, State)
-      Header: "Location",
-      accessor: "location",
-    },
-    {
-      // Renders actions that user can perform, such as deleting or updating
-      Header: " ",
-      Cell: ({ row }) => (
-        <HStack spacing={4} justify="end">
-          <Tooltip label="EditIcon Member" hasArrow>
-            <IconButton
-              icon={<EditIcon />}
-              colorScheme="blue"
-              size="sm"
-            />
-          </Tooltip>
-          <Tooltip label="Delete Member" hasArrow>
-            <IconButton
-              icon={<DeleteIcon />}
-              colorScheme="red"
-              size="sm"
-            />
-          </Tooltip>
-        </HStack>
-      ),
-    },
-    
-    
-  ],
-  []
-)
-
-return (
-  <>
-  <AddMemberPop />
-    
-    <Flex direction="row">
-      {/* Table */}
-      <Box
-        bg="white"
-        borderRadius={8}
-        border="1px"
-        borderColor="gray.50"
-        w="100%"
-      >
-        {/* Table generated with React-Table */}
-        <MemberTable columns={columns} data={members} />
-      </Box>
-    </Flex>
-  </>
-)
-}
-
-
-export default Members;
+export default Members
