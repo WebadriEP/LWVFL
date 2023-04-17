9f50c340b4535f12d6cd28c10e4224d3fe924f88
ryandey <techrpd15113@gmail.com>
2023-04-17 18:18:09 -0400

    Attempts to fix imports

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,24 +1,34 @@
 const express = require("express")
 const multer = require("multer")
 const router = express.Router()
-const upload = multer()
 const csv = require("csvtojson")
 const memberModel = require("../models/memberModel")
 
-// Get request to /api/upload to test the upload route
-router.get("/", (req, res) => {
-  res.send("Upload route")
+// body parser
+//srouter.use(express.json())
+
+const upload = multer({
+  storage: storage,
+})
+
+const storage = multer.diskStorage({
+  // Where the file will be saved
+  destination: (req, file, callback) => {
+    callback(null, "/uploads")
+  },
+
+  // Rename the file
+  filename: (req, file, callback) => {
+    callback(null, `${Date.now()}_${file.originalname}`)
+  },
+
+  inMemory: true,
 })
 
 // Post request to /api/upload to upload a file
 router.post("/", upload.single("file"), async (req, res) => {
-  const {
-    file,
-    body: { name },
-  } = req
-
-  // Convert CSV to JSON
-  const json = await csv().fromFile(file)
+  const file = req.body
+  const json = await csv().fromFile(file) // Convert CSV to JSON
 
   // Send to MongoDB
   memberModel.insertMany(json, (err, result) => {
